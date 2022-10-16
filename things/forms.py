d0d788b93772ae40f653d7616d335485e8f656d7
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-16 23:45:30 +0100

    Add ThingForm form to things app

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
+from django import forms
+
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.Form):
+  name = forms.CharField(label='Name', max_length=35)
+  description = forms.CharField(label='Description', widget=forms.Textarea)
+  quantity = forms.IntegerField(label='Quantity')
+
