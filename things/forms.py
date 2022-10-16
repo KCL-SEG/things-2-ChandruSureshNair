79bbda152cbd59371870b6c3be810d84b9f43563
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-17 00:01:17 +0100

    Implement Thing creation

--- things/forms.py
+++ things/forms.py
@@ -5,6 +5,6 @@ from django import forms
 # Create your forms here.
 class ThingForm(forms.Form):
   name = forms.CharField(label='Name', max_length=35)
-  description = forms.CharField(label='Description', widget=forms.Textarea)
-  quantity = forms.IntegerField(label='Quantity')
+  description = forms.CharField(label='Description', widget=forms.Textarea, max_length=120)
+  quantity = forms.IntegerField(label='Quantity', min_value=0, max_value=50)
 
--- things/views.py
+++ things/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .forms import ThingForm
+from .models import Thing
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            quantity = form.cleaned_data['quantity']
+            thing = Thing(name=name, description=description, quantity=quantity)
+            thing.save()
+            form = ThingForm()
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': ThingForm()})
